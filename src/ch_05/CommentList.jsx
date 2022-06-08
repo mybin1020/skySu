a7eb7556ce7395df694f399eff86c717889eebc0
subin <mybin1020@gmail.com>
2022-06-08 21:37:38 +0900

    first

--- src/ch_05/CommentList.jsx
+++ src/ch_05/CommentList.jsx
@@ -3,7 +3,7 @@ import Comment from "./Comment";
 
 const comments = [
   { name: "subin", content: "react 하기 싫어" },
-  { name: "skyboy", content: "공부나 해" },
+  { name: "skyboy", content: "전 바보입니다" },
 ];
 
 function CommentList(props) {
